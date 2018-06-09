925140d1c1762d4d1d6fe02ba4fd0a705af527ba
eladFrizi <okc.elad.35@gmail.com>
2018-06-10 01:21:58 +0300

    added support for super mode

--- js/main.js
+++ js/main.js
@@ -11,19 +11,15 @@ import {
 } from './directionService.js';
 
 import render from './render.js'
-const {
-  pipe,
-  assoc
-} = R
+const { pipe, assoc, partial, equals, find, without } = R
 
 const SIMPLE_FOOD = '.'
 const SUPER_FOOD = '$'
 
 const getGhost = ({ i, j }) => {
-  var dir = getRandomDirection()
   return {
     type: 'ghost',
-    direction: dir,
+    direction: getRandomDirection(),
     i,
     j
   }
@@ -36,26 +32,15 @@ const getCell = ({ i, j }) =>
   ({
     isWall: false,
     food: '',
-    character: '',
     i,
     j
   })
 
-const getWallCell = pipe(getCell,assoc('isWall', true))
-const getSimpleFoodCell = pipe(getCell,assoc('food', SIMPLE_FOOD))
-const getSuperFoodCell = pipe(getCell,assoc('food', SUPER_FOOD))
+const getWallCell = pipe(getCell, assoc('isWall', true))
+const getSimpleFoodCell = pipe(getCell, assoc('food', SIMPLE_FOOD))
+const getSuperFoodCell = pipe(getCell, assoc('food', SUPER_FOOD))
 
-
-// const getSuperFoodCell = ({ i, j }) => {
-//   const cell = getCell({
-//     i,
-//     j
-//   })
-//   cell.food = SUPER_FOOD
-//   return cell
-// }
-
-const isWallCord = ({ i, j, size }) =>
+const isWallCell = ({ i, j, size }) =>
   (i === 0 || i === size - 1 || j === 0 || j === size - 1)
 
 const placeSuperFoodAtCorners = (board) => {
@@ -76,16 +61,16 @@ const createBoard = (size) => {
   for (var i = 0; i < size; i++) {
     board[i] = []
     for (var j = 0; j < size; j++) {
-      if (isWallCord({ i, j, size })) {
+      if (isWallCell({ i, j, size })) {
         board[i][j] = getWallCell({ i, j })
       } else {
         board[i][j] = getSimpleFoodCell({ i, j })
       }
     }
   }
-  console.log(board)
-  board[10][1] = getWallCell({ i, j })
-  board[10][19] = getWallCell({ i, j })
+  board[10][1] = getWallCell({ i: 10, j: 1 })
+  board[10][19] = getWallCell({ i: 10, j: 19 })
+  board[11][11] = getWallCell({ i: 11, j: 11 })
 
 
   placeSuperFoodAtCorners(board)
@@ -96,21 +81,40 @@ const createBoard = (size) => {
 const getInitialGhosts = (count, board) => {
   return Array.from({
     length: count
-  }, (v, i) => i)
-    .map(x => pipe(getGhostStartLocation, getGhost)(board))
+  }, pipe(
+    R.partial(getGhostStartLocation,[board]), 
+    getGhost
+  ))
 }
+
 const getInitialPacman = pipe(getPacmanStartLocation, getPacman)
+
+const countFood = (board) => {
+  var count = 0
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      if (board[i][j].food) count++
+    }
+  }
+  return count
+}
 
 const initState = x => {
   const board = createBoard(21)
+  const foodCount = countFood(board)
   const ghosts = getInitialGhosts(3, board)
   const pacman = getInitialPacman(board)
   const isGameOver = false;
-  const isPowerMode = false
-  return { board, ghosts, pacman, isGameOver, isPowerMode }
+  const score = 0;
+  const powerModeSteps = 200;
+  const deadGhosts = []
+  return { board, ghosts, pacman, isGameOver, powerModeSteps, score, deadGhosts, foodCount }
 }
 
-
+const checkIsPowerModeOn = R.pipe(
+  R.prop('powerModeSteps'),
+  R.partialRight(R.gt, [0]),
+)
 
 const isValidCell = ((board, { i, j }) => {
   if (i < 0 || i >= board.length) return false
@@ -118,8 +122,8 @@ const isValidCell = ((board, { i, j }) => {
   return !board[i][j].isWall
 })
 
-const moveGhosts = (board, ghosts) => {
-  return ghosts.map(g => {
+const moveGhosts = ({ board, ...state }) => {
+  const ghosts = state.ghosts.map(g => {
     var newLoc = getNextLocation(g)
     if (isValidCell(board, newLoc)) return { ...g, ...newLoc }
     // debugger
@@ -137,25 +141,89 @@ const moveGhosts = (board, ghosts) => {
     var newLoc = getNextLocation({ i: g.i, j: g.j, direction: newDir })
     return { ...newGhost, ...newLoc, direction: newDir }
   })
+  return { ...state, board, ghosts }
 }
 
 const findGhostInPacmanPlace = ({ pacman, ghosts }) => {
   return ghosts.find(g => g.i === pacman.i && g.j === pacman.j)
 }
 
-const runGameCycle = () => {
-  gState.ghosts = moveGhosts(gState.board, gState.ghosts)
-  var ghostInPacman = findGhostInPacmanPlace(gState)
-  if (ghostInPacman) {
-    if(gState.isPowerMode) {
-      //TODO: Kill ghost
-    } else {
-      //TODO: game over.
-      gState.isGameOver = true;
-    }
-  }
+const changePacmanLocation = (state, direction) => {
+  state.pacman = { ...state.pacman, direction }
+}
 
-  render.renderGame(gState)
+const movePacman = (state) => {
+  var newLoc = getNextLocation(state.pacman)
+  let pacman = (isValidCell(state.board, newLoc))
+    ? { ...state.pacman, ...newLoc } : state.pacman
+  return { ...state, pacman }
+}
+
+const eatFood = ({ score, powerModeSteps, ...state }) => {
+  const { i, j } = state.pacman
+  const currCell = state.board[i][j]
+  if (currCell.food === SIMPLE_FOOD) {
+    score += 1
+  } else if (currCell.food === SUPER_FOOD) {
+    score += 10
+    state.powerModeSteps = 50
+  }
+  currCell.food = ''
+  return { score, powerModeSteps, ...state }
+}
+
+const killGhost = (ghostInPacman, state) => {
+  console.log('inside kil gohotst')
+  if (!state.powerModeSteps) return state
+  const checkNotEaten = R.pipe(R.equals(ghostInPacman), R.not)
+  const liveGhosts = (R.filter(checkNotEaten, state.ghosts))
+  const deathCount = state.ghosts.length - liveGhosts.length
+  const deadGhosts = R.concat(state.deadGhosts, R.repeat(ghostInPacman, deathCount))
+  return { ...state, ghosts: liveGhosts, deadGhosts }
+}
+
+const checkEngagement = (state) => {
+  var ghostInPacman = findGhostInPacmanPlace(state)
+  if (!ghostInPacman) return state
+
+  return R.ifElse(
+    checkIsPowerModeOn,
+    R.partial(killGhost, [ghostInPacman]),
+    R.tap(s => console.log('TODO: Finish the game'))
+  )(state)
+}
+
+const decPowerModeSteps = ({ powerModeSteps, ...state }) => {
+  powerModeSteps -= 1
+  return { ...state, powerModeSteps }
+}
+const isPowerModeJustFinish = (state) => R.equals(0, state.powerModeSteps)
+
+const revivalGhosts = (state) => {
+  console.log(state.deadGhosts)
+  console.log('hey world')
+  console.log(getInitialGhosts(state.deadGhosts.length, state.board))
+  return {
+    ...state,
+    ghosts: [...state.ghosts, 
+            ...getInitialGhosts(state.deadGhosts.length, state.board)],
+    deadGhosts: []
+  }
+}
+
+const runGameCycle = () => {
+  // each function get the state and return new state.
+  gState = R.pipe(
+    moveGhosts,
+    checkEngagement,
+    movePacman,
+    checkEngagement,
+    eatFood,
+    decPowerModeSteps,
+    R.when(isPowerModeJustFinish, revivalGhosts),
+    R.tap(console.log),
+    R.tap(render.renderGame)
+  )(gState)
 }
 
 
@@ -169,7 +237,36 @@ document.querySelector('.next-cycle').addEventListener('click', x => {
   runGameCycle()
 })
 document.querySelector('.next-cycle-5').addEventListener('click', x => {
-  for (var i = 0; i < 5; i++) {
-    runGameCycle()
+  runCycles(5);
+})
+
+document.querySelector('.next-cycle-100').addEventListener('click', x => {
+  runCycles(100);
+})
+document.querySelector('.next-cycle-50').addEventListener('click', x => {
+  runCycles(50);
+})
+
+
+document.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+      changePacmanLocation(gState, 'left')
+      break;
+    case "ArrowRight":
+      changePacmanLocation(gState, 'right')
+      break;
+    case "ArrowUp":
+      changePacmanLocation(gState, 'up')
+      break;
+    case "ArrowDown":
+      changePacmanLocation(gState, 'down')
+      break;
   }
 })
+
+function runCycles(num) {
+  for (var i = 0; i < num; i++) {
+    runGameCycle();
+  }
+}
--- js/render.js
+++ js/render.js
@@ -1,56 +1,66 @@
-const getCellClass = ({i,j}) => `cell-${i}-${j}`
+const getCellClass = ({ i, j }) => `cell-${i}-${j}`
 
-const getCellHTML = (cell,i,j) => {
-    var content = cell.isWall ? '=' : cell.food
-    return (
-      `<td class="cell-${i}-${j}">
+const getCellHTML = (cell, i, j) => {
+  var content = cell.isWall ? '=' : cell.food
+  return (
+    `<td class="cell-${i}-${j}">
         ${content}
       </td>`
-    )
-  }
+  )
+}
 
-
-
-
-  const renderBoard = board => {
-    const elTbody = document.querySelector('tbody');
-    const strHTML = board.map((row,i) =>
-          `<tr>
-              ${row.map((cell,j) => getCellHTML(cell,i,j)).join('')}
+const renderBoard = board => {
+  const elTbody = document.querySelector('tbody');
+  const strHTML = board.map((row, i) =>
+    `<tr>
+              ${row.map((cell, j) => getCellHTML(cell, i, j)).join('')}
           </tr>`
-    ).join('')
-    elTbody.innerHTML = strHTML
+  ).join('')
+  elTbody.innerHTML = strHTML
+}
+
+const renderGhosts = ghosts => {
+  const elTbody = document.querySelector('tbody');
+  ghosts.forEach(g => {
+    const cellClass = getCellClass(g)
+    const elCell = elTbody.querySelector('.' + cellClass)
+    elCell.innerHTML = 'G'
   }
+  )
 
-  const renderGhosts = ghosts => {
-    const elTbody = document.querySelector('tbody');
-    ghosts.forEach(({i,j, ...ghost}) => {
-        const cellClass = getCellClass({i,j})
-        const elCell = elTbody.querySelector('.'+cellClass)
-        elCell.innerHTML = 'G'
-    })
+}
+
+const renderPacman = pacman => {
+  const elTbody = document.querySelector('tbody');
+  const cellClass = getCellClass(pacman)
+  const elCell = elTbody.querySelector('.' + cellClass)
+  elCell.innerHTML = 'P'
+}
+
+const  renderEl = selector => {
+  const el = document.querySelector(selector) 
+  return (content) => {
+      el.innerHTML = content
   }
+}
+const renderPowerModeSteps = renderEl('.steps')
 
-  const renderPacman = ({i,j, ...pacman}) => {
-    const elTbody = document.querySelector('tbody');
-    const cellClass = getCellClass({i,j})
-    const elCell = elTbody.querySelector('.'+cellClass)
-    elCell.innerHTML = 'P'
-  }
 
-  const renderGame = ({board, ghosts, pacman}) => {
-    renderBoard(board)
-    renderGhosts(ghosts)
-    renderPacman(pacman)
-  }
 
-  
+const renderGame = ({ board, ghosts, pacman, powerModeSteps }) => {
+  renderBoard(board)
+  renderGhosts(ghosts)
+  renderPacman(pacman)
+  renderPowerModeSteps(powerModeSteps)
+}
 
-  
 
-  export default {
-      renderBoard,
-      renderGhosts,
-      renderPacman,
-      renderGame
-  }
+
+
+
+export default {
+  renderBoard,
+  renderGhosts,
+  renderPacman,
+  renderGame
+}
